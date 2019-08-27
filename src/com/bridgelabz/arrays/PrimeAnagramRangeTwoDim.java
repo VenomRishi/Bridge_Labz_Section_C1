c746b0faabeb3bbda3bbf1d93cc18167b3628fef
Slot2 <mhatrerishikesh@gmail.com>
2019-08-27 16:06:01 +0530

    Prime number two dimension program is done

---
+++ src/com/bridgelabz/arrays/PrimeAnagramRangeTwoDim.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.arrays;
+
+public class PrimeAnagramRangeTwoDim {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/com/bridgelabz/utility/ArraysUtility.java
+++ src/com/bridgelabz/utility/ArraysUtility.java
@@ -107,4 +107,18 @@ public class ArraysUtility {
 		}
 	}
 
+	public static boolean isPrime(int num) {
+		if (num == 0 || num == 1)
+			return false;//
+		else {
+			for (int i = 2; i < num / 2; i++) {
+				if (num % i == 0) {
+					return false;//
+				}
+			}
+		}
+
+		return true;
+	}
+
 }
