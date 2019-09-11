98265516215ccbf26447174b929e15caac0ff4d1
VenomRishi <mhatrerishikesh@gmail.com>
2019-09-11 11:49:01 +0530

    magic number is added

--- src/com/bridgelabz/search/MagicNumber.java
+++ src/com/bridgelabz/search/MagicNumber.java
@@ -17,14 +17,11 @@ import com.bridgelabz.utility.SearchUtility;
 public class MagicNumber {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 
 		int n = Integer.parseInt("3");
 		int range = (int) Math.pow(2, n);
 		System.out.println("Range is 0 to " + range);
 		SearchUtility.magicNumber(range);
-		
-		scanner.close();
 
 	}
 
